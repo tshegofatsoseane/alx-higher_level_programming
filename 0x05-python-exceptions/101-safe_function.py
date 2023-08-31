ace563fe8d80cf2a6113e4ea0728fb63f03b3f13
tshegofatsoseane <tshegofatsoseane98@gmail.com>
2023-08-31 00:15:40 +0000

    Final files

---
+++ 0x05-python-exceptions/101-safe_function.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+
+import sys
+
+
+def safe_function(fct, *args):
+    """Execute function safely.
+
+    Args:
+        fct: The executed function.
+        args: fct args.
+
+    Returns:
+            Results
+    """
+    try:
+        result = fct(*args)
+        return (result)
+    except:
+        print("Exception: {}".format(sys.exc_info()[1]), file=sys.stderr)
+        return (None)
