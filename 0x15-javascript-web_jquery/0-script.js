3eed6d6639f44b5cfb2b3ec7f76210ba644bf3c9
tshegofatsoseane <tshegofatsoseane98@gmail.com>
2024-01-11 03:48:29 +0000

    update text color

---
+++ 0x15-javascript-web_jquery/0-script.js
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="UTF-8">
+    <title>Change Text Color</title>
+  </head>
+  <body>
+    <header>
+      This is the header text
+    </header>
+
+    <script>
+
+      var headerElement = document.querySelector('header');
+
+
+      headerElement.style.color = '#FF0000';
+    </script>
+  </body>
+</html>
